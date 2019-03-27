7e079b03f32afb68fc9f3cea9f59d7c19458c9e3
JonathanPlasse <jonathan.plasse@live.fr>
2019-03-27 19:52:53 +0100

    Docoumentation to check, ready to test

--- binserial.py
+++ binserial.py
@@ -5,6 +5,7 @@
 
 TODO:
 - Finish Documentation.
+- Catch potential exceptions
 - Implement multi-treading.
 
 """
@@ -13,7 +14,9 @@ __all__ = ['BinSerial']
 __version__ = '1.0'
 __author__ = 'Jonathan Plasse'
 
+# For serial connection
 import serial
+# To convert binary data
 import struct
 import time
 
@@ -23,70 +26,82 @@ class BinSerial:
 
     def __init__(self, port_name, baud_rate):
         """Open a serial link at port_name with baud_rate."""
-        self.types_dict = {'char': 'c', 'bool': '?',
-                           'int8': 'b', 'uint8': 'B',
-                           'int16': 'h', 'uint16': 'H',
-                           'int32': 'i', 'uint32': 'I',
-                           'int64': 'l', 'uint64': 'L',
-                           'float': 'f'}
+        # Dict to convert explicit types to format caracters
+        self.format_dict = {'char': 'c', 'bool': '?',
+                            'int8': 'b', 'uint8': 'B',
+                            'int16': 'h', 'uint16': 'H',
+                            'int32': 'i', 'uint32': 'I',
+                            'int64': 'l', 'uint64': 'L',
+                            'float': 'f'}
+        # Port name of the serial port
         self.port_name = port_name
+        # Baud rate of the connection
         self.baud_rate = baud_rate
 
         # Open serial link
         self.ser = serial.Serial(self.port_name, self.baud_rate, timeout=1)
 
-        # Wait for the arduino to initialize
+        # Wait for initialisation
         time.sleep(2)
 
     def __del__(self):
         """Close serial link."""
         self.ser.close()
 
-    def _compute_format(self, structFormat):
-        """Compute the format string for struct."""
-        structTypes = '='
+    def _compute_format(self, struct_format):
+        """Compute the format caracters to convert binary data."""
+        # Set byte order to native and no alignement for the binary data
+        format_caracters = '='
 
-        for t in structFormat:
-            structTypes += self.types_dict[t]
+        # Convert struct_format explicit types to format caracters
+        for t in struct_format:
+            format_caracters += self.format_dict[t]
 
-        return structTypes
+        return format_caracters
 
-    def read(self, structFormat):
-        """Read data from serial link with structFormat."""
-        structTypes = self.compute_format(structFormat)
-        nbBytes = struct.calcsize(structTypes)
+    def read(self, struct_format):
+        """Read data from serial link with struct_format."""
+        # Compute the format caracters
+        format_caracters = self.compute_format(struct_format)
+        # Calculate the number of bytes needed
+        nb_bytes = struct.calcsize(format_caracters)
 
         # Wait until all the data is in the buffer
-        while self.ser.in_waiting < nbBytes:
+        while self.ser.in_waiting < nb_bytes:
             pass
 
         # Read the raw data
-        rawData = bytearray(nbBytes)
-        self.ser.readinto(rawData)
+        raw_data = bytearray(nb_bytes)
+        self.ser.readinto(raw_data)
 
         # Convert the raw data
-        data = list(struct.unpack(structTypes, rawData))
+        data = list(struct.unpack(format_caracters, raw_data))
 
         return data
 
-    def write(self, structFormat, data):
-        """Write data to serial link with structFormat."""
-        structTypes = self.compute_format(structFormat)
-        rawData = struct.pack(structTypes, *data)
-        self.ser.write(rawData)
+    def write(self, struct_format, data):
+        """Write data to serial link with struct_format."""
+        # Compute the format caracters
+        format_caracters = self.compute_format(struct_format)
+
+        # Convert data in binary
+        raw_data = struct.pack(format_caracters, *data)
+        # Send raw_data
+        self.ser.write(raw_data)
 
 
 if __name__ == '__main__':
     port_name = '/dev/ttyACM0'
     baud_rate = 115200
 
-    # Define the format of the structure of data sent
-    structFormatSent = ['float']*2+['int16']
-    structFormatReceived = ['float']*2+['int16']
+    # Define the format of the structure of the data
+    struct_format = ['float']*2+['int16']
 
+    # Initialize connection
     bser = BinSerial(port_name, baud_rate)
 
-    bser.write(structFormatSent, [2.718, 3.14, 5203])
-    data = bser.read(structFormatReceived)
-
+    # Test echo
+    bser.write(struct_format, [2.718, 3.14, 5203])
+    data = bser.read(struct_format)
+    
     print(data)
